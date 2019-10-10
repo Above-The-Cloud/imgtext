c1281682b5d0498f7ec65f3967fbe05d00772b35
yiwangchunyu <yiwangchunyu@163.com>
2019-10-10 23:51:56 +0800

    articles views

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+
+class Article(models.Model):
+    cover = models.CharField(max_length=200)
+    desc = models.TextField()
+    text = models.TextField()
+    source = models.IntegerField()
+    org_id = models.IntegerField()
+    link = models.CharField(max_length=200)
+    author = models.CharField(max_length=200,blank=True, null=True)
+    category1 = models.IntegerField(blank=True, null=True)
+    category2 = models.IntegerField(blank=True, null=True)
+    rstatus = models.IntegerField(default=1, blank=True)
+    meta1 = models.IntegerField(blank=True, null=True)
+    meta2 = models.CharField(max_length=200,blank=True, null=True)
+    meta3 = models.CharField(max_length=400,blank=True, null=True)
+    status = models.IntegerField(default=1, blank=True)
+    ctime = models.DateTimeField(default = timezone.now)
+    mtime = models.DateTimeField(auto_now = True)
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,82 @@
+import json
+import traceback
+
+from django.core import serializers
 from django.http import HttpResponse
-from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+from articles.models import Article
+
+
 def hello(request):
     return HttpResponse("Hello world ! ")
+
+@csrf_exempt
+def create(request):
+    res = {'code': 0, 'msg': 'success', 'data': {}}
+    # if not {'user_id','title','label','content'}.issubset(request.POST.keys()):
+    #     return HttpResponse(json.dumps({'code': -1, 'msg': 'error-1|unexpected params!', 'data': json.dumps(request.POST.dict())}))
+    try:
+        params = request.POST.dict()
+        dis=Article.objects.create(**params)
+        res['data']['id']=dis.id
+    except:
+        traceback.print_exc()
+        return HttpResponse(json.dumps({'code': -2, 'msg': 'error-2', 'data': []}))
+    return HttpResponse(json.dumps(res))
+
+@csrf_exempt
+def list(request):
+    res = {'code': 0, 'msg': 'success', 'data': {}}
+    try:
+        params = request.POST.dict()
+        page=0
+        size=20
+        if 'page' in params:
+            page=params['page']
+            params.pop('page')
+        if 'size' in params:
+            size=params['size']
+            params.pop('size')
+        params['status']=1
+        res['data']['count']=Article.objects.filter(**params).count()
+        res['data']['articles']=[]
+        qset=Article.objects.filter(**params).order_by('-ctime')[page*size:(page+1)*size]
+        articles=json.loads(serializers.serialize("json", qset))
+
+        for article in articles:
+            data_row=article['fields']
+            data_row['id']=article['pk']
+            del data_row['status']
+            res['data']['article'].append(data_row)
+    except:
+        traceback.print_exc()
+        return HttpResponse(json.dumps({'code': -2, 'msg': 'error-2', 'data': []}))
+    return HttpResponse(json.dumps(res))
+
+@csrf_exempt
+def update(request):
+    res = {'code': 0, 'msg': 'success', 'data': []}
+    if not {'id','update'}.issubset(request.POST.keys()):
+        return HttpResponse(json.dumps({'code': -1, 'msg': 'error-1|unexpected params!', 'data': []}))
+    try:
+        Article.objects.filter(id=request.POST['id']).update(**json.loads(request.POST['update']))
+    except:
+        traceback.print_exc()
+        return HttpResponse(json.dumps({'code': -2, 'msg': 'error-2', 'data': []}))
+    return HttpResponse(json.dumps(res))
+
+
+@csrf_exempt
+def delete(request):
+    res = {'code': 0, 'msg': 'success', 'data': []}
+    if not {'id'}.issubset(request.POST.keys()):
+        return HttpResponse(json.dumps({'code': -1, 'msg': 'error-1|unexpected params!', 'data': []}))
+    try:
+        Article.objects.filter(id=request.POST['id']).update(status=0)
+    except:
+        traceback.print_exc()
+        return HttpResponse(json.dumps({'code': -2, 'msg': 'error-2', 'data': []}))
+    return HttpResponse(json.dumps(res))
